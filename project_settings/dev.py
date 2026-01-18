b9a7591e85d2c622685088e7313db1eb17187298
gitanshugupta7 <gitanshugupta77@gmail.com>
2026-01-18 19:48:04 +0530

    minot font and card fixes

--- project_settings/dev.py
+++ project_settings/dev.py
@@ -3,7 +3,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='127.0.0.1,localhost')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='127.0.0.1,localhost,10.32.117.165')
 
 # Optional: Add django-debug-toolbar for development
 # INSTALLED_APPS += ['debug_toolbar']
